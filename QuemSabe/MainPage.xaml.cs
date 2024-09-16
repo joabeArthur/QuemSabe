e0c68e0547b276f9a7570b0581bcaa0470e1bda1
joabe <joalokk@gmail.com>
2024-09-16 09:04:17 -0300

    Sabo um pouco

--- QuemSabe/MainPage.xaml.cs
+++ QuemSabe/MainPage.xaml.cs
@@ -2,23 +2,38 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	public string Perguntas;
+	public string Resposta1;
+	public string Resposta2;
+	public string Resposta3;
+	public string Resposta4;
+	public int RespostaCorreata { get; set; }
+
+//==================================================================\\
+	private int NivelId { get; set; }
+	private int Resposta1Id { get; set; }
+	private int Resposta2Id { get; set; }
+	private int Resposta3Id { get; set; }
+	private int Resposta4Id { get; set; }
+	private int RespostaCorretaId { get; set; }
+
+//==================================================================\\
+
+	List<pergunta> pergunta = new List<pergunta>;  
+
+//==================================================================\\
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+		pergunta.Add("Quantas linhas tem cu?");
+		pergunta.Add("Vacas conseguem subir escadas?");
+		pergunta.Add("Quem se mexer é gay?")
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+//==================================================================\\
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
