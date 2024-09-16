a421e0e24854d5b9a468aa5d46c285d769a9fbc3
joabe <joalokk@gmail.com>
2024-09-16 07:54:15 -0300

    Ainda não sei

---
+++ QuemSabe/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace QuemSabe;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ QuemSabe/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QuemSabe;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
